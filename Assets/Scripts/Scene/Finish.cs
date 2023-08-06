fc0ada1cb4da2fedb33464b4f9e7811d77811ed8
Amaury Couturier <amo.couturier@gmail.com>
2023-08-06 18:45:54 -0400

    UI touchup
    Changed fonts, got scene switching to work correctly and just overall improved the UI

--- Assets/Scripts/Scene/Finish.cs
+++ Assets/Scripts/Scene/Finish.cs
@@ -7,11 +7,30 @@ public class Finish : MonoBehaviour
 {
     private ShootPistol shootPistol; 
 
+    private void Start()
+    {
+        // Initialize the shootPistol object by finding the ShootPistol component on the PlayerObject
+        shootPistol = GameObject.Find("RayGun").GetComponent<ShootPistol>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "PlayerObject" && shootPistol.enemiesLeft == 0)
-            LevelManager.manager.FinishedGame();
-        else if(other.gameObject.name == "PlayerObject" && shootPistol.enemiesLeft != 0)
-            SceneManager.LoadScene("Game Over");
+        if(other.gameObject.name == "PlayerObject")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            
+            if (shootPistol != null)
+            {
+                if (shootPistol.enemiesLeft == 0)
+                    LevelManager.manager.FinishedGame();
+                else
+                    SceneManager.LoadScene("Game Over");
+            }
+            else
+            {
+                Debug.LogError("ShootPistol component not found.");
+            }
+        }
     }
 }
